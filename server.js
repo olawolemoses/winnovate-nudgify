bc5b8e1bf66a5038e9ee60b6807abb4068902a37
olawole-moses Ogunleye <olawolemoses@gmail.com>
2025-09-09 18:05:43 +0100

    Added push notification service worker

--- server.js
+++ server.js
@@ -3,15 +3,29 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error loading index.html');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        }
-    });
+    // Handle the service worker file
+    if (req.url === '/firebase-messaging-sw.js') {
+        fs.readFile(path.join(__dirname, 'firebase-messaging-sw.js'), (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Not Found');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                res.end(data);
+            }
+        });
+    } else {
+        // Serve the index.html file for all other requests
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error loading index.html');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
