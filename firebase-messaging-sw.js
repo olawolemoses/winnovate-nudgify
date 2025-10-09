20a791f10d83159c8451285ec338dd3918bd8121
olawole-moses Ogunleye <olawolemoses@gmail.com>
2025-10-09 11:00:53 +0100

    fixes made

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // Minimal service worker that defers logic to Nudgify's hosted script
 const swLocation = new URL(self.location.href);
-const backendUrl = "https://prod.nudgify.io";
+const backendUrl = "https://prod-api.nudgify.io";
 const siteId = swLocation.searchParams.get("site_id");
 
 const remoteScript = new URL("/widget/push-sw.js", backendUrl.replace(/\/$/, ""));
